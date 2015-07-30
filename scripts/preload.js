941d3dab94ca8cb4fa24d5617708162dc20d819d
ni4ka7a <n.atanasow@yahoo.com>
2015-07-29 20:29:01 -0700

    Add 6 new sprites and load them in preload() function

--- scripts/preload.js
+++ scripts/preload.js
@@ -9,6 +9,12 @@ preload.prototype = {
         loadingBar.anchor.setTo(0.5,0.5);
         this.load.setPreloadSprite(loadingBar);
         this.game.load.spritesheet("dog","resources/images/dog.png",30,24);
+        this.game.load.spritesheet("turtle","resources/images/turtleSpriteRight.png",85,66);
+        this.game.load.spritesheet("camel","resources/images/camalSprite.png",80,57);
+        this.game.load.spritesheet("fox","resources/images/foxSpriteRight.png",48,29);
+        this.game.load.spritesheet("blueBird","resources/images/blueBirdLeft.png",109,82);
+        this.game.load.spritesheet("raccoon","resources/images/raccoonSpriteLeft.png",32,18);
+        this.game.load.spritesheet("turtleLeft","resources/images/turtleSpriteLeft.png",85,64);
         this.game.load.image("background","resources/images/bg.jpg");
         this.game.load.image("gametitle","resources/images/Title.png");
         this.game.load.image("play","resources/images/playButton.jpg");
